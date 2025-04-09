ed8f3ee5ba693e04376c3ae6efaabd8a93da80e6
Joan M. Richetti <joanrichetti1@gmail.com>
2025-04-09 16:06:02 -0400

    Add files via upload

---
+++ WebScraping.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+#///////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/#
+
+
+response = requests.get("https://quotes.toscrape.com/")
+soup = BeautifulSoup(response.text, 'html.parser')
+
+divs = soup.select('div[Class="quote"]')
+for div in divs:
+    print()
+
+if len(divs) > 0:
+    # Imprimir el texto del primer div
+    print("Primer div:", divs[0].text.strip())
+
+if len(divs) > 1:
+    # Imprimir el texto del primer div
+    print("Primer div:", divs[1].text.strip())
+
+if len(divs) > 2:
+    # Imprimir el texto del primer div
+    print("Primer div:", divs[2].text.strip())
